b3f8ba426bc36c506ae3cede1b8d4be4e148d5de
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-07-15 21:40:35 -0400

    Moved most of the package metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,58 +1,14 @@
 #!/usr/bin/env python
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-# -*- encoding: utf-8 -*-
 
-import io
-import re
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
-from setuptools import setup, find_packages
+import sys
+import setuptools
+from distutils.version import LooseVersion
+from setuptools import setup
 
+# Setuptools 30.3.0 or later is needed for setup.cfg options to be used
+if LooseVersion(setuptools.__version__) < LooseVersion('30.3.0'):
+    sys.stderr.write("ERROR: sphinx-automodapi requires setuptools 30.3.0 or "
+                     "later (found {0})".format(setuptools.__version__))
+    sys.exit(1)
 
-def readme():
-    with open('README.rst') as ff:
-        return ff.read()
-
-
-setup(
-    name='pytest-openfiles',
-    version='0.4.0.dev',
-    license='BSD',
-    description='Pytest plugin for detecting inadvertent open file handles',
-    long_description=readme(),
-    author='The Astropy Developers',
-    author_email='astropy.team@gmail.com',
-    url='https://astropy.org',
-    packages=find_packages(exclude=['tests']),
-    include_package_data=True,
-    zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 3 - Alpha',
-        'Framework :: Pytest',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Utilities',
-    ],
-    keywords=[ 'detect', 'open', 'file', 'handle', 'psutil', 'pytest', 'py.test' ],
-    install_requires=[ 'pytest>=2.8.0', 'psutil' ],
-    python_requires='>=2.7',
-    entry_points={
-        'pytest11': [
-            'pytest_openfiles = pytest_openfiles.plugin',
-        ],
-    },
-)
+setup()
